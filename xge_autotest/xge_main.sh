1e426cda705b13e079e8fa7dee524e74cf596b39
wanghaifeng0825 <191476419@qq.com>
2018-02-02 17:46:37 +0800

    hns Test script update

--- xge_autotest/xge_main.sh
+++ xge_autotest/xge_main.sh
@@ -8,43 +8,46 @@ XGE_TOP_DIR=$(cd "`dirname $0`" ; pwd)
 . ${XGE_TOP_DIR}/config/xge_test_lib
 
 # Load the public configuration library
-. ${XGE_TOP_DIR}/../config/common_config
-. ${XGE_TOP_DIR}/../config/common_lib
+#. ${XGE_TOP_DIR}/../config/common_config
+#. ${XGE_TOP_DIR}/../config/common_lib
 
 
 # Main operation function
 # IN : N/A
 # OUT: N/A
+
 function main()
 {
-    Module_Name="XGE"
-    
-    for key in "${!case_map[@]}"
+    cat ${TEST_CASE_DB_FILE} | while read line
     do
-        case "${case_map[$key]}" in
-            on)
-                commd="${key}.sh"
-                source ${XGE_TOP_DIR}/case_script/$commd
-            ;;
-            off)
-            ;;
-            *)
-                echo "xge_test_config file test case flag parameter configuration error."
-                echo "please configure on and off."
-                echo "on  - open test case."
-                echo "off - close test case."
-            ;;
-       esac
+        exec_script=`echo "${line}" | awk -F '\t' '{print $5}'`
+        TEST_CASE_FUNCTION_NAME=`echo "${line}" | awk -F '\t' '{print $6}'`
+        TEST_CASE_FUNCTION_SWITCH=`echo "${line}" | awk -F '\t' '{print $7}'`
+
+        if [ x"${exec_script}" == x"" ]
+        then
+            MESSAGE="unimplemented automated test cases."
+        else
+            if [ ! -f "case_script/${exec_script}" ]
+            then
+                MESSAGE="case_script/${exec_script} execution script does not exist, please check."
+            else
+                source case_script/${exec_script}
+            fi
+        fi
+        echo -e "${line}\t${MESSAGE}" >> ${OUTPUT_TEST_DB_FILE}
+        MESSAGE=""
     done
 }
+
+#ifconfig IP
 init_net_ip
-init_net_export
 
-# Output log file header
-writeLogHeader
-
-# 
+#set passwd
 setTrustRelation
+
+#ifconfig net export
+init_net_export
 
 main
 
