21c0f3c7d183042c61e6cc93ceda54f71db819ef
Luojiaxing1991 <luojiaxing@huawei.com>
2018-02-03 17:41:53 +0800

    enable phy_control_throuth sysfs

--- sas_autotest/case_script/phy_control_through_sysfs.sh
+++ sas_autotest/case_script/phy_control_through_sysfs.sh
@@ -8,30 +8,32 @@ function set_rate_link()
     for dir in `ls ${PHY_FILE_PATH}`
     do
         type=`cat ${PHY_FILE_PATH}/${dir}/target_port_protocols`
+	echo "Begin to check sas type in "${dir}
         if [ x"$type" != x"none" ]
         then
+            echo "Begin to set rate in "${dir}
             #this function will destroy the setting value of disk speed
 	    #so may be we should save the value first,then recove it after test
-            tmp_min=`cat ${PHY_FILE_PATH}/${dir}/minimum_linkrate | awk "{printf $0}"`
-	    echo $tmp_min
-            tmp_max=`cat ${PHY_FILE_PATH}/${dir}/maximum_linkrate | awk "{printf $0}"`
-	    echo $tmp_max
-            #echo $MINIMUM_LINK_VALUE > ${PHY_FILE_PATH}/${dir}/minimum_linkrate
-	    cat ${PHY_FILE_PATH}/${dir}/minimum_linkrate
+            tmp_min=`cat ${PHY_FILE_PATH}/${dir}/minimum_linkrate | awk '{printf $0}'`
+	    echo "origin min rate value is "$tmp_min
+            tmp_max=`cat ${PHY_FILE_PATH}/${dir}/maximum_linkrate | awk '{printf $0}'`
+	    echo "origin max rate value is"$tmp_max
 
-            #echo $MAXIMUM_LINK_VALUE > ${PHY_FILE_PATH}/${dir}/maximum_linkrate
-            #[ $? -ne 0 ] && MESSAGE="FAIL\tFailed to set the maximum rate of \"${dir}\" greater than the minimum rate." && return 1
+            echo $MINIMUM_LINK_VALUE > ${PHY_FILE_PATH}/${dir}/minimum_linkrate
+            echo $MAXIMUM_LINK_VALUE > ${PHY_FILE_PATH}/${dir}/maximum_linkrate
+            [ $? -ne 0 ] && MESSAGE="FAIL\tFailed to set the maximum rate of \"${dir}\" greater than the minimum rate." && return 1
 
-            #echo $MINIMUM_LINK_VALUE > ${PHY_FILE_PATH}/${dir}/maximum_linkrate
-            #[ $? -ne 0 ] && MESSAGE="FAIL\tFailed to set the \"${dir}\" maximum rate equal to the minimum rate." && return 1
+            echo $MINIMUM_LINK_VALUE > ${PHY_FILE_PATH}/${dir}/maximum_linkrate
+            [ $? -ne 0 ] && MESSAGE="FAIL\tFailed to set the \"${dir}\" maximum rate equal to the minimum rate." && return 1
 
-            #echo $MAXIMUM_LINK_VALUE > ${PHY_FILE_PATH}/${dir}/minimum_linkrate
-            #[ $? -eq 0 ] && MESSAGE="FAIL\tFailed to set the \"${dir}\" maximum rate less than the minimum rate." && return 1
+            echo $MAXIMUM_LINK_VALUE > ${PHY_FILE_PATH}/${dir}/minimum_linkrate
+            [ $? -eq 0 ] && MESSAGE="FAIL\tFailed to set the \"${dir}\" maximum rate less than the minimum rate." && return 1
 
-            #echo $tmp_min > ${PHY_FILE_PATH}/${dir}/minimum_linkrate
+	    echo "Begin to recove the origin value of max and min rate"
+            echo $tmp_min > ${PHY_FILE_PATH}/${dir}/minimum_linkrate
+	    echo $tmp_max > ${PHY_FILE_PATH}/${dir}/minimum_linkrate
 	    cat ${PHY_FILE_PATH}/${dir}/minimum_linkrate
             cat ${PHY_FILE_PATH}/${dir}/maximum_linkrate
-
 
         fi
         sleep 5
