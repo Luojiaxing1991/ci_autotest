529a90de34a70124817eaa124d5eb9e3d92740e6
Luojiaxing1991 <luojiaxing@huawei.com>
2018-02-06 10:56:51 +0800

    lava-test-case is not oK

--- perf_autotest/perf_main.sh
+++ perf_autotest/perf_main.sh
@@ -17,20 +17,28 @@ PERF_TOP_DIR=$(cd "`dirname $0`" ; pwd)
 function main()
 {
     cat ${TEST_CASE_DB_FILE} | while read line
+
+    echo "Begin to run PERF test!"
+
     do
         exec_script=`echo "${line}" | awk -F '\t' '{print $5}'`
         TEST_CASE_FUNCTION_NAME=`echo "${line}" | awk -F '\t' '{print $6}'`
         TEST_CASE_FUNCTION_SWITCH=`echo "${line}" | awk -F '\t' '{print $7}'`
+        
+        #Get the test title from testcase.table
+	TEST_CASE_TITLE=`echo "${line}" | awk -F '\t' '{print $3}'`
+
+        echo "CaseInfo "${TEST_CASE_TITLE}" "${TEST_CASE_FUNCTION_SWITCH}
 
         if [ x"${exec_script}" == x"" ]
         then
             MESSAGE="unimplemented automated test cases."
         else
-            if [ ! -f "case_script/${exec_script}" ]
+            if [ ! -f "${PERF_TOP_DIR}/case_script/${exec_script}" ]
             then
                 MESSAGE="FILE\tcase_script/${exec_script} execution script does not exist, please check."
             else
-                source case_script/${exec_script}
+                source ${PERF_TOP_DIR}/case_script/${exec_script}
             fi
         fi
         echo -e "${line}\t${MESSAGE}" >> ${OUTPUT_TEST_DB_FILE}
