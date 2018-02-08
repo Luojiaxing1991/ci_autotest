d0a19266f27f034c1fdec849c3124225740f4295
zhangxiping <gnsy0845zxp@sohu.com>
2018-02-08 09:08:56 +0800

    RoCE: Split the Load_Unload_kernel_driver into Load_kernel_driver and Unload_kernel_driver, and modify according to the latest framework.

--- roce_autotest/case_script/Load_Unload_kernel_driver.sh
+++ roce_autotest/case_script/Load_Unload_kernel_driver.sh
@@ -1,69 +1,61 @@
 #!/bin/bash
 
-#load and unload roce's kernel driver
+#load roce's kernel driver
 #IN	:N/A
 #OUT:N/A
-function Load_Unload_kernel_driver()
+function Load_kernel_driver()
 {
-	Test_Case_ID="ST-ROCE-87"
-	Test_Case_Title="Load the RoCE's kernel driver"
-	insmod hns-roce.ko > ${FUNCNAME}.log
-	if [ $? != 0 ]
+	if [ -d /sys/class/infiniband/hns_0 ]
 	then
-		writeFail "insmod hns-roce.ko failed, please check !"
-		return 1
+		MESSAGE="PASS\tThe kernel driver already exists"
+		return 0
 	fi
 
+	insmod hns-roce.ko > ${FUNCNAME}.log
+	[ $? != 0 ] && MESSAGE="FAIL\tinsmod hns-roce.ko failed"; return 1
+
 	insmod hns-roce-hw-v1.ko >> ${FUNCNAME}.log
-	if [ $? != 0 ]
-	then
-		writeFail "insmod hns-roce-hw-v1.ko failed, please check !"
-		return 1
-	fi
+	[ $? != 0 ] && MESSAGE="FAIL\tinsmod hns-roce-hw-v1.ko failed"; return 1
 
 	if [ -d /sys/class/infiniband/hns_0 ]
 	then
-		writePass "Load RoCE's kernel driver successfully"
+		MESSAGE="PASS"
 	else
-		writeFail "Load RoCE's kernel driver failed, please check ${FUNCNAME}.log"
-		return 1
+		MESSAGE="FAIL\tregister the RoCE's devices failed, please check ${FUNCNAME}.log"
 	fi
+}
 
-	Test_Case_ID="ST-ROCE-88"
-	Test_Case_Title="Unload the RoCE's kernel driver"
+#unload roce's kernel driver
+#IN :N/A
+#OUT:N/A
+function Unload_kernel_driver()
+{
+	lsmod > ${FUNCNAME}.log
+	grep -w "hns_rocehw_v1" ${FUNCNAME}.log
+	[ $? != 0 ] && MESSAGE="BLOCK\tModule hns_roce_hw_v1 is not currently loaded"; return 1
+
 	rmmod hns-roce-hw-v1.ko >> ${FUNCNAME}.log
-	if [ $? != 0 ]
-	then
-		writeFail "rmmod hns-roce-hw-v1.ko failed, please check !"
-		return 1
-	fi
+	[ $? != 0 ] && MESSAGE="FAIL\trmmod hns-roce-hw-v1.ko failed, please check log"; return 1
+
+	grep -w "hns_roce" ${FUNCNAME}.log
+	[ $? != 0 ] && MESSAGE="BLOCK\tModule hns_roce is not currently loaded"; return 1
 
 	rmmod hns-roce.ko >> ${FUNCNAME}.log
-	if [ $? != 0 ]
-	then
-		writeFail "rmmod hns-roce.ko failed, please check !"
-		return 1
-	fi
+	[ $? != 0 ] && MESSAGE="FAIL\trmmod hns-roce.ko failed, please check log"; return 1
 
 	if [ ! -d /sys/class/infiniband/hns_0 ]
 	then
-		writePass "Unload RoCE's kernel driver successfully"
+		MESSAGE="PASS"
 	else
-		writeFail "Unload RoCE's kernel driver failed, please check ${FUNCNAME}.log"
-		return 1
+		MESSAGE="FAIL\tUnload RoCE's kernel driver failed, please check ${FUNCNAME}.log"
 	fi
 
-	return 0
 }
 
 function main()
 {
-	JIRA_ID=""
-	Designed_Requirement_ID=""
-	Test_Case_ID=""
-	Test_Item="Verify load/remove the RoCE's kernel driver "
-	Test_Case_Title=""
-	Load_Unload_kernel_driver
+	# call the implementation of the automation use cases
+	test_case_function_run
 }
 
 main
