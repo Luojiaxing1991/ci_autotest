f4ca543f184f8e6975d72ed1fd1e3a3d520f431b
Luojiaxing1991 <luojiaxing@huawei.com>
2018-02-11 16:05:23 +0800

    update pcie test

--- pcie_autotest/case_script/runcmd.sh
+++ pcie_autotest/case_script/runcmd.sh
@@ -1,21 +1,182 @@
-#!/bin/bash
-RunCmds()
-{
-    varDicCols=${1}
-    nCols=${2}
-    varDicValues=${3}
-    varFlLog=${4}
-}
 
-#fio --name=read --rw=read --bs=1M --runtime=30 --filename=/dev/nvme0n1 --numjobs=32 --iodepth=128 --direct=1 --sync=0 --norandommap --group_reporting --time_based
-sCmd1='fio --name=${dic[0]} --rw=${dic[0]} --bs=${dic[1]} --runtime=30 --filename=/dev/nvme0n1 --numjobs=32 --iodepth=128 --direct=1 --sync=0 --norandommap --group_reporting --time_based'
-
-m_dicValue=(
-    [0]=read
-    [1]=1M
-)
-
-RunCmds dic 2 m_dicValue m_flLog
+g_flParent="${BASH_SOURCE[1]}"
+g_fnCur=$(basename "${BASH_SOURCE[0]}")
+g_dpCur=$(dirname  "${BASH_SOURCE[0]}")
+drCur0=${PWD}
+cd "${g_dpCur}" >/dev/null
+g_dpCur=${PWD}
+cd "${drCur0}" >/dev/null
+g_pfnCur=${g_dpCur}/${g_fnCur}
+export g_fn_runcmd_common=${g_fnCur}
+export g_dp_runcmd_common=${g_dpCur}
 
 #####################
+#check load from command line
+if [ -z "${g_flParent}" ]; then
+    #if at the command line, support reload the source file
+    #delete it first
+    g_defSourceFiles=$(grep -vF "${g_pfnCur}" <<< "${g_defSourceFiles}")
+    #bug: export g_defSourceFiles=${g_defSourceFiles}${g_pfnCur}$'\n'
+    #will delete the end line \n
+fi
+
+#check loaded already
+sPF1=$(grep -F "${g_pfnCur}" <<< "${g_defSourceFiles}")
+if [ $? -eq 0 ]; then
+    #then [ "${sPF1}" == "${g_pfnCur}" ]
+    #path file name is only one in file system.
+	return 100
+fi
+
+export g_defSourceFiles=${g_defSourceFiles}$'\n'${g_pfnCur}
+printf "%s[%3d]%5s: Load [${g_pfnCur}] call by [${g_flParent}]\n" "${FUNCNAME[0]}" ${LINENO} "Info"
+
+####################################################################################
+RunFio()
+{
+    local tRW=${1}
+    local bsU=${2}
+    local nfPerfDef=${3}
+    local nTimesRun=${4}
+    local sPosName=${5}
+    local flLog=${6:-/dev/null}
+
+    declare -A \
+    g_dicLib1_B136=(
+        [kDrLoad]=${g_dp_pcie_test_common}
+	    [kMDrLog]='${HOME}/tmp/data/logs'
+    )
+
+    #fio --name=read --rw=read --bs=1M --runtime=30 --filename=/dev/nvme0n1 --numjobs=32 --iodepth=128 --direct=1 --sync=0 --norandommap --group_reporting --time_based
+    local m_sCmd
+    m_sCmd='false'
+    m_sCmd='fio --name=${m_dic[0]} --rw=${m_dic[0]} --bs=${m_dic[1]} --runtime=30 --filename=/dev/nvme0n1 --numjobs=32 --iodepth=128 --direct=1 --sync=0 --norandommap --group_reporting --time_based'
+
+    local nCols=2
+    local m_sTrapRuned
+    declare -A m_dicValue=()
+    local nR1 nC1
+
+    #####################
+    nR1=0
+    nC1=0
+    while [ ${nR1} -lt ${nTimesRun} ]; do
+        m_dicValue[${nR1}_${nC1}]="${tRW}"
+        let nC1+=1
+
+        m_dicValue[${nR1}_${nC1}]="${bsU}"
+        let nC1+=1
+
+        if [ ${nC1} -ge ${nCols} ]; then
+            nC1=0
+        fi
+
+        let nR1+=1
+    done
+
+    #####################
+    local drLocalLogs
+    drLocalLogs=${g_dicLib1_B136[kMDrLog]}
+    drLocalLogs=${HOME}/tmp/data/logs
+    SafeRemoveFolder "/logs/" "${drLocalLogs}/${g_dicMCSame[kDrTmpLog]}" "${flLog}"
+
+    m_sTrapRuned="
+    "
+    RunACmdsRemote g_dicLib1_B136 "pcie_test_lib" "${m_sCmd}" m_dic 2 m_dicValue 1 m_sTrapRuned true "${flLog}"
+
+    #####################
+    local sFlList s1 sUnit1 nPosV nPerf1
+
+    sFlList=$(find "${drLocalLogs}/${g_dicMCSame[kDrTmpLog]}" -maxdepth 1 -type f)
+    if [ -z "${sFlList}" ]; then
+        g_sMsgCur="no data files in [${drLocalLogs}/${g_dicMCSame[kDrTmpLog]}]"
+        g_sHeadCurLine=$(printf "%s[%3d]%s[%3d]" "${FUNCNAME[1]}" "${BASH_LINENO[0]}" "${FUNCNAME[0]}" ${LINENO})
+        OutLogHead 1 "" "${g_sHeadCurLine}" "${g_sMsgCur}" "${flLog}" false
+        return 1
+    fi
+
+    IFS=$'\n' #解决文件名有空格;
+    s1=$(sed -n "/^[ \t]*${tRW}[ \t]*:/{n;/^[ \t]*\(read\|write\)[ \t]*:/p}" ${sFlList})
+    IFS=${g_IFS0}
+    echo "${s1}"
+
+    case "${sPosName}" in
+    bw)
+        sUnit1=M
+        nPosV=2
+        ;;
+    iops)
+        sUnit1=[KM]
+        nPosV=3
+        ;;
+    *)
+        g_sMsgCur="not know position name [${sPosName}]"
+        g_sHeadCurLine=$(printf "%s[%3d]%s[%3d]" "${FUNCNAME[1]}" "${BASH_LINENO[0]}" "${FUNCNAME[0]}" ${LINENO})
+        OutLogHead 1 "" "${g_sHeadCurLine}" "${g_sMsgCur}" "${flLog}" false
+        return 1
+    esac
+
+    #read : io=75794MB, bw=2525.1MB/s, iops=2525, runt= 30006msec
+    nPerf1=$(sed -n "s#^[ \t]*\(read\|write\)[ \t]*:[^,]\+, bw=\([0-9.]\+\)${sUnit1}B/s, iops=\([0-9]\+\), .*\$#\\${nPosV}#p" <<< "${s1}")
+    if [ -z "${nPerf1}" ]; then
+        g_sMsgCur="not found data, data files or unit or key not correct?"
+        g_sHeadCurLine=$(printf "%s[%3d]%s[%3d]" "${FUNCNAME[1]}" "${BASH_LINENO[0]}" "${FUNCNAME[0]}" ${LINENO})
+        OutLogHead 1 "" "${g_sHeadCurLine}" "${g_sMsgCur}" "${flLog}" false
+        return 1
+    fi
+
+    #####################
+    local nMax1 nR1 sR1 nReach sRMax nStat1
+    nMax1=$(sed -n '$=' <<< "${nPerf1}")
+    nR1=1
+    sR1=$(sed -n "${nR1}p" <<< "${nPerf1}")
+    nReach=0
+    FloatCmp "${sR1}" "${nfPerfDef}"
+    if [ $? -lt 2 ]; then
+        let nReach+=1
+    fi
+    sRMax=${sR1}
+    while [ ${nR1} -lt ${nMax1} ]; do
+        let nR1+=1
+        sR1=$(sed -n "${nR1}p" <<< "${nPerf1}")
+        FloatCmp "${sR1}" "${nfPerfDef}"
+        if [ $? -lt 2 ]; then
+            let nReach+=1
+        fi
+        FloatCmp "${sR1}" "${sRMax}"
+        if [ $? -lt 2 ]; then
+            sRMax=${sR1}
+        fi
+    done
+
+    #####################
+    let nStat1=\!nReach
+    g_sMsgCur="${tRW} max value[${sRMax}], reach ${nfPerfDef} times[${nReach}]"
+    g_sHeadCurLine=$(printf "%s[%3d]%s[%3d]" "${FUNCNAME[1]}" "${BASH_LINENO[0]}" "${FUNCNAME[0]}" ${LINENO})
+    OutLogHead "${nStat1}" "" "${g_sHeadCurLine}" "${g_sMsgCur}" "${flLog}" false
+
+    #####################
+    mkdir -p "${drLocalLogs}/${g_dicMCSame[kDrProperLog]}"
+    mv "${drLocalLogs}/${g_dicMCSame[kDrTmpLog]}"/* "${drLocalLogs}/${g_dicMCSame[kDrProperLog]}"
+
+    #####################
+    return ${nStat1}
+}
+
+#####################
+RunFioUser()
+{
+    local tRW=${1}
+    local nfPerfDef=${2}
+    local nTimesRun=${3:-3}
+
+    case "${tRW}" in
+    read|write)
+        RunFio "${tRW}" 1m "${nfPerfDef}" "${nTimesRun}" bw "${g_flLog}"
+        ;;
+    randread|randwrite)
+        RunFio "${tRW}" 4k "${nfPerfDef}" "${nTimesRun}" iops "${g_flLog}"
+        ;;
+    esac
+}
 
